aa294e00056492d5534bca477dfe30c05f7302f8
mcheco <pachecomurilo133@gmail.com>
2022-11-26 15:35:33 -0300

    fisrt commit

---
+++ script.js
@@ -0,0 +1,6 @@
+let num1 = parseInt(prompt("Digite um numero para que seja feita a subtração."));
+let num2 = parseInt(prompt("Digite um numero para que seja feita a subtração."));
+
+let resultado = num1 - num2;
+
+alert(`${num1} - ${num2} = ${resultado}`)
